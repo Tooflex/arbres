e379425b8908da680f04070d15fc5e891c292957
Otourou Da Costa <otouroudacosta@gmail.com>
2022-01-11 21:45:49 +0100

    Initial Commit

---
+++ Arbres One Point/Arbres_One_PointApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arbres_One_PointApp.swift
+//  Arbres One Point
+//
+//  Created by Otourou Da Costa on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Arbres_One_PointApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
